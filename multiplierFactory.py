eb413c0ca7f97150cc5c655ebff31831c129b49e
Rodrigo Rivera <rodrigo.rivera.7186@gmail.com>
2025-05-08 22:36:18 -0600

    Agg multiplierfactory.py

---
+++ multiplierFactory.py
@@ -0,0 +1,19 @@
+class MultiplierFactory:
+    
+    def __new__(cls, factor: int):
+        print(f"Creando instancia con factor {factor}")
+        return super(MultiplierFactory, cls).__new__(cls)
+    
+    def __init__(self, factor: int):
+        print(f"Inicializando con factor {factor}")
+        self.factor = factor
+    
+    def __call__(self, number: int) -> int:
+        print(f"El número será 10: {number}")
+        return number * self.factor
+        
+    
+multiplier = MultiplierFactory(5)
+
+result = multiplier(10)
+print(result)
