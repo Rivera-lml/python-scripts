94c903367f29f48ef95a0c3de8fb92ec445b02fe
Rodrigo Rivera <rodrigo.rivera.7186@gmail.com>
2025-05-10 10:40:14 -0600

    Agg __new__

---
+++ example__new__.py
@@ -0,0 +1,24 @@
+class Vehicle:
+    def __new__(cls, wheels: int):
+        if wheels == 2:
+            return Motorbike()
+        elif wheels == 4:
+            return Car()
+        else:
+            return super().__new__(cls)
+        
+    def __init__(self, wheels: int):
+        self.wheels = wheels
+        print(f'Initializing vehicle with {wheels} wheels')
+
+class Motorbike:
+    def __init__(self):
+        print('Initializing motorbike')
+
+class Car:
+    def __init__(self):
+        print('Initializing car')
+
+mb = Vehicle(2)
+
+bus = Vehicle(10)
