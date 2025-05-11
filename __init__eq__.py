7b148fcec913847301af8478369a0a4fa3fdd81b
Rodrigo Rivera <rodrigo.rivera.7186@gmail.com>
2025-05-11 11:58:18 -0600

    Agg __init__eq__

---
+++ __init__eq__.py
@@ -0,0 +1,12 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def __eq__(self, otra_persona):
+        return self.nombre == otra_persona.nombre and self.edad == otra_persona.edad
+
+p1 = Persona("Ana", 30)
+p2 = Persona("Ana", 30)
+
+print(p1 == p2)  # True: both have the same name and age
